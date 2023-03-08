6de8c5f6d565a889e5574bb6cccd4d4f98cbcb6c
Abulohaa <82264457+Abulohaa@users.noreply.github.com>
2023-03-09 01:41:47 +0300

    footer fix

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -11,23 +11,24 @@ import { Router } from '@angular/router';
 })
 export class FooterComponent implements OnInit {
   constructor(private router: Router) { }
-  cart$: Observable<ShoppingCart>;
-  shoppingCartService: ShoppingCartService;
-  subscription: Subscription;
-  cart: ShoppingCart;
+  // cart$: Observable<ShoppingCart>;
+  // shoppingCartService: ShoppingCartService;
+  // subscription: Subscription;
+  // cart: ShoppingCart;
 
-  async ngOnInit(): Promise<void> {
-    this.cart$ = await this.shoppingCartService.getCart();
-    debugger
+  // async ngOnInit(): Promise<void> {
+  //   this.cart$ = await this.shoppingCartService.getCart();
+  //   debugger
 
-    this.subscription = this.cart$.subscribe(res => {
-      debugger
-      this.cart = res;
-      // console.log(this.cart);
-    })
-  }
+  //   this.subscription = this.cart$.subscribe(res => {
+  //     debugger
+  //     this.cart = res;
+  //     // console.log(this.cart);
+  //   })
+  // }
   setPosition(): boolean {
-    return (this.router.url.includes("shopping-cart") || this.router.url.includes("my/orders")) && this.cart.totalItemsCount == 0;
+    return (this.router.url.includes("shopping-cart") || this.router.url.includes("my/orders"))
+    //  && this.cart.totalItemsCount == 0;
     // && this.cart$.totalItemsCount == 0);
   }
 }
